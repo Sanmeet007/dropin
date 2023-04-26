6b623b128820bcbffa9207e57b4a510f720b8d65
Sanmeet007 <ssanmeet123@gmail.com>
2023-04-26 21:15:43 +0530

    updated fields in job

--- models/job.js
+++ models/job.js
@@ -9,14 +9,20 @@ class JobDetails {
   /** @type {Date} */ created_at;
   /** @type {string} */ description;
   /** @type {string} */ title;
-  /** @type {string} */ client_profile_image;
+  /** @type {string?} */ client_profile_image;
   /** @type {boolean} */ is_user_verified;
   /** @type {string} */ client_email;
   /** @type {string} */ client_name;
+  /** @type {Array<string>?} */ required_skillset;
+  /** @type {Date} */ closed_at;
+  /** @type {Date} */ created_at;
 
   constructor(d) {
+    this.created_at = d.created_at;
+    this.closed_at = d?.closed_at ?? null;
     this.uid = d.user_id;
     this.id = d.job_id;
+    this.required_skillset = d?.skillset?.split(",") ?? null;
     this.client_details = new ClientDetails({
       client_id: d.client_id,
       company_name: d.company_name,
@@ -30,7 +36,7 @@ class JobDetails {
     this.budget = parseFloat(d.budget);
     this.title = d.title;
     this.description = d.description;
-    this.client_profile_image = d.profle_image;
+    this.client_profile_image = d?.profle_image ?? null;
     this.is_user_verified = d.verified;
     this.client_email = d.email;
     if (d.last_name) {
